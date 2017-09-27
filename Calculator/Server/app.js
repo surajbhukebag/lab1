616c6318fa227eca439ef6192324b4dc07ceaafe
Suraj Bhukebag <surajbhukebag@gmail.com>
2017-09-27 11:43:44 -0700

    Adding CORS support for services

--- Calculator/Server/app.js
+++ Calculator/Server/app.js
@@ -8,12 +8,13 @@ var express = require('express')
   , user = require('./routes/user')
   , http = require('http')
   , path = require('path')
+  ,	cors = require('cors')
   , calculator = require('./routes/calculator');
 
 var app = express();
 
 // all environments
-app.set('port', process.env.PORT || 3000);
+app.set('port', process.env.PORT || 3001);
 app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs');
 app.use(express.favicon());
@@ -23,6 +24,9 @@ app.use(express.methodOverride());
 app.use(app.router);
 app.use(express.static(path.join(__dirname, 'public')));
 
+//Enable CORS
+app.use(cors());
+
 // development only
 if ('development' == app.get('env')) {
   app.use(express.errorHandler());
@@ -30,10 +34,10 @@ if ('development' == app.get('env')) {
 
 app.get('/', calculator.home);
 
-app.post('/add', calculator.add);
-app.post('/subtract', calculator.subtract);
-app.post('/multiply', calculator.multiply);
-app.post('/divide', calculator.divide);
+app.post('/add', cors(), calculator.add);
+app.post('/subtract', cors(),calculator.subtract);
+app.post('/multiply',cors(), calculator.multiply);
+app.post('/divide', cors(),calculator.divide);
 
 
 http.createServer(app).listen(app.get('port'), function(){
